58c772acf334f28105ae6ed4afd5fd94e3dd0707
emanuele <me.emanuele.xyz@gmail.com>
2024-06-12 12:34:04 +0200

    start implementing player registration with admin server

--- src/main/java/watchout/Player.java
+++ src/main/java/watchout/Player.java
@@ -1,5 +1,7 @@
 package watchout;
 
+import com.sun.jersey.api.client.Client;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,15 +11,18 @@ public class Player {
     private static int id = 0;
     private static int port = 0;
     private static String adminServerAddress = null;
+    private static String adminServerPlayersEndpoint = null;
+    private static String adminServerHeartbeatsEndpoint = null;
+    private static Client restClient = null;
 
-    private static void initializePlayer() throws IOException {
+    private static boolean initializePlayer() throws IOException {
         System.out.print("ID > ");
         String idStr = keyboard.readLine().trim().toLowerCase();
         try {
             id = Integer.parseInt(idStr);
         } catch (NumberFormatException e) {
             System.out.println("Invalid ID '" + idStr + "'");
-            return;
+            return false;
         }
 
         System.out.print("Port > ");
@@ -26,16 +31,27 @@ public class Player {
             port = Integer.parseInt(portStr);
         } catch (NumberFormatException e) {
             System.out.println("Invalid port '" + portStr + "'");
-            return;
+            return false;
         }
 
         System.out.print("Admin server address > ");
         adminServerAddress = keyboard.readLine().trim().toLowerCase();
+        adminServerPlayersEndpoint = adminServerAddress + "/players";
+        adminServerHeartbeatsEndpoint = adminServerAddress + "/heartbeats";
+
+        return true;
+    }
+
+    private static void registerPlayer() {
+        restClient = Client.create();
+        // TODO: to be implemented
     }
 
     public static void main(String[] args) throws IOException {
         keyboard = new BufferedReader(new InputStreamReader(System.in));
 
-        initializePlayer();
+        boolean isInitializationSuccessful = initializePlayer();
+        if (!isInitializationSuccessful) return;
+        registerPlayer();
     }
 }
