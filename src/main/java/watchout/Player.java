3c24471f4c79b40ce63e42d518687ba4e929fdb7
emanuele <me.emanuele.xyz@gmail.com>
2024-06-12 12:08:23 +0200

    implement player initialization

--- src/main/java/watchout/Player.java
+++ src/main/java/watchout/Player.java
@@ -1,7 +1,36 @@
 package watchout;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Player {
-    public static void main(String[] args) {
-        System.out.println("Hello from player");
+    private static BufferedReader keyboard = null;
+
+    public static void main(String[] args) throws IOException {
+        keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.print("ID > ");
+        String idStr = keyboard.readLine().trim().toLowerCase();
+        int id = 0;
+        try {
+            id = Integer.parseInt(idStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid ID '" + idStr + "'");
+            return;
+        }
+
+        System.out.print("Port > ");
+        String portStr = keyboard.readLine().trim().toLowerCase();
+        int port = 0;
+        try {
+            port = Integer.parseInt(portStr);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid port '" + portStr + "'");
+            return;
+        }
+
+        System.out.print("Admin server address > ");
+        String serverAddress = keyboard.readLine().trim().toLowerCase();
     }
 }
