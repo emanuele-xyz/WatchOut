5b0b291df4fcf0ae1dc36084b77ab5c02565d32a
emanuele <me.emanuele.xyz@gmail.com>
2024-06-12 12:11:08 +0200

    pull player initialization code into a method

--- src/main/java/watchout/Player.java
+++ src/main/java/watchout/Player.java
@@ -6,13 +6,13 @@ import java.io.InputStreamReader;
 
 public class Player {
     private static BufferedReader keyboard = null;
+    private static int id = 0;
+    private static int port = 0;
+    private static String adminServerAddress = null;
 
-    public static void main(String[] args) throws IOException {
-        keyboard = new BufferedReader(new InputStreamReader(System.in));
-
+    private static void initializePlayer() throws IOException {
         System.out.print("ID > ");
         String idStr = keyboard.readLine().trim().toLowerCase();
-        int id = 0;
         try {
             id = Integer.parseInt(idStr);
         } catch (NumberFormatException e) {
@@ -22,7 +22,6 @@ public class Player {
 
         System.out.print("Port > ");
         String portStr = keyboard.readLine().trim().toLowerCase();
-        int port = 0;
         try {
             port = Integer.parseInt(portStr);
         } catch (NumberFormatException e) {
@@ -31,6 +30,12 @@ public class Player {
         }
 
         System.out.print("Admin server address > ");
-        String serverAddress = keyboard.readLine().trim().toLowerCase();
+        adminServerAddress = keyboard.readLine().trim().toLowerCase();
+    }
+
+    public static void main(String[] args) throws IOException {
+        keyboard = new BufferedReader(new InputStreamReader(System.in));
+
+        initializePlayer();
     }
 }
